0ac06ca0c5834f9ea676d6f4f2d1c4e696159451
Nick Sergeychuk <nicknixer@gmail.com>
2017-09-17 23:27:23 +0300

    cats has been saved

--- test.js
+++ test.js
@@ -1,28 +1,28 @@
 const Zombie = require('zombie');
 const should = require('should');
-var assert = require('assert');
+const assert = require('assert');
 
 const data = [ 
-{"value": "il@gmail.com", "expected": "no"}, 
-{"value": "ilya@gmail.com", "expected": "yes"}, 
-{"value": "elias@gmail.ru", "expected": "no"}, 
-{"value": "il-ya@gmail.com", "expected": "no"}, 
-{"value": "name#gmail.com", "expected": "no"}, 
-{"value": "hahaha@gmail.com", "expected": "yes"},
-{"value": "hahaha@.com", "expected": "no"} 
+    {"value": "il@gmail.com", "expected": "no"}, 
+    {"value": "ilya@gmail.com", "expected": "yes"}, 
+    {"value": "elias@gmail.ru", "expected": "no"}, 
+    {"value": "il-ya@gmail.com", "expected": "no"}, 
+    {"value": "name#gmail.com", "expected": "no"}, 
+    {"value": "hahaha@gmail.com", "expected": "yes"},
+    {"value": "hahaha@.com", "expected": "no"} 
 ];
 
 
 describe('Email checker', function() {
     for (const o of data) {
-		it(`${o.value} should be returned ${o.expected}`, function(done) {
-			this.timeout(15000);
-    		Zombie.visit(`https://kodaktor.ru/g/testing_0ab37?test=${o.value}`, (e, browser)=>{
-    			browser.pressButton('go').then(function() {
-			      assert.ok(browser.success);
-			      assert.equal(browser.text('h3'), o.expected);
-			    }).then(done, done);
-    		});
-  		});
-	}
+        it(`${o.value} should be returned ${o.expected}`, function(done) {
+            this.timeout(15000);
+            Zombie.visit(`https://kodaktor.ru/g/testing_0ab37?test=${o.value}`, (e, browser)=>{
+                browser.pressButton('go').then(function() {
+                    assert.ok(browser.success);
+                    assert.equal(browser.text('h3'), o.expected);
+                }).then(done, done);
+            });
+        });
+    }
 });

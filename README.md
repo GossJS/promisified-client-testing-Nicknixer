e2d5b5b18b7b12671dcb3878b8237d41e092e13c
Nick Sergeychuk <nicknixer@gmail.com>
2017-09-17 18:15:05 +0300

    added tests on mocha and Zombie

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 
 1. Страница с проверкой адреса https://kodaktor.ru/g/testing_0ab37?test=asdsad@gmail.com
+2. Для запуска теста:
+
+$ yarn
+$ yarn test
 
 
 
--- test.js
+++ test.js
@@ -1,12 +1,32 @@
-import Zombie from 'zombie';
-import should from 'should';
-const URL = 'https://kodaktor.ru/g/testing_f38ac';
+const Zombie = require('zombie');
+const should = require('should');
+const URL = 'http://kodaktor.ru';
+var assert = require('assert');
+
+var Browser = require('zombie');
 const page = new Zombie();
 const g=URL=>new Promise((rs,rj)=>page.visit(URL,e=>(e)?rj(e):rs()));
-const data = [ {"value": "il@gmail.com", "expected": "no"}, {"value": "ilya@gmail.com", "expected": "yes"}, {"value": "elias@gmail.ru", "expected": "no"}, {"value": "il-ya@gmail.com", "expected": "no"}, {"value": "name#gmail.com", "expected": "no"}, {"value": "hahaha@gmail.com", "expected": "yes"} ];
-for (const o of data) {
-  it('should return yes or no', ()=>{
-    g(URL+'?test='+o.value).then(()=>({result: page.document.querySelector('h3#response').textContent})).should
-    .eventually.have.property('result',  o.value );
-  });
-}
+const data = [ 
+{"value": "il@gmail.com", "expected": "no"}, 
+{"value": "ilya@gmail.com", "expected": "yes"}, 
+{"value": "elias@gmail.ru", "expected": "no"}, 
+{"value": "il-ya@gmail.com", "expected": "no"}, 
+{"value": "name#gmail.com", "expected": "no"}, 
+{"value": "hahaha@gmail.com", "expected": "yes"},
+{"value": "hahaha@.com", "expected": "no"} 
+];
+
+
+describe('Email checker', function() {
+    for (const o of data) {
+		it(`${o.value} should be returned ${o.expected}`, function(done) {
+			this.timeout(15000);
+    		Zombie.visit(`https://kodaktor.ru/g/testing_0ab37?test=${o.value}`, (e, browser)=>{
+    			browser.pressButton('go').then(function() {
+			      assert.ok(browser.success);
+			      assert.equal(browser.text('h3'), o.expected);
+			    }).then(done, done);
+    		});
+  		});
+	}
+});
